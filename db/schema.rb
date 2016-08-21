c8c69d0fb9ca24711ca8d95d6199445434a68ece
Leona Hu <hqleona@gmail.com>
2016-08-20 21:13:07 -0700

    added user authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160819060815) do
+ActiveRecord::Schema.define(version: 20160821033926) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
     t.string   "string"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "user_id"
+    t.string   "integer"
+    t.string   "index"
   end
 
   create_table "users", force: :cascade do |t|
