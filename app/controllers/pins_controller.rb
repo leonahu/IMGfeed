edd4eb5b92c269c75f543e85d7488a898e7fb975
Leona Hu <hqleona@gmail.com>
2016-08-20 21:44:50 -0700

    refrine user authorization

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
-
+  before_action :correct_user, only: [:edit, :update, :destroy]
   def index
     @pins = Pin.all
   end
@@ -41,9 +41,14 @@ class PinsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_pin
-      @pin = Pin.find(params[:id])
+      @pin = Pin.find_by(id: params[:id])
+      redirect_to pins_path, notice:"Not authorized to edit this pin" if @pin.nil?
     end
 
+
+    def correct_user
+      @pin = current_user.pins.find(params[:id])
+    end
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
       params.require(:pin).permit(:description)
