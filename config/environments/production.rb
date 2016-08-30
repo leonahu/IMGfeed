a50d40ac0d32c13905b065ad4281c7e9dfebe7e8
Leona Hu <hqleona@gmail.com>
2016-08-30 12:23:03 -0700

    removed aws keys

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,9 +94,9 @@ end
 config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
-    bucket: ENV.fetch('bibliophilist'),
-    access_key_id: ENV.fetch('AKIAJGAY3KA3AJNLW3MA'),
-    secret_access_key: ENV.fetch('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'),
-    s3_region: ENV.fetch('Oregon'),
+    bucket: ENV.fetch('S3_BUCKET_NAME'),
+    access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    s3_region: ENV.fetch('AWS_REGION'),
   }
 }
